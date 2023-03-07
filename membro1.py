cbc642a99c92a2b96a91547bfc7ce3f2083c7e0b
unknown <artursodar@gmail.com>
2023-03-07 20:56:53 -0300

    membro1

--- membro1.py
+++ membro1.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o Artur Luiz, tenho 18 anos, meus hobbies são jogos onlines e series. Tenho experiencia em python e uma nocao basica em C++. Espero desta disciplina entender mais sobre Programacao Orientada a Objeto, e melhorar meu trabalho em grupo." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o membro1." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
